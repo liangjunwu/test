47e1e1ab3bc8c943c625beb8672de646a162d5fa
liangjunwu <1847280382@qq.com>
2018-01-08 15:22:15 +0800

    创建项目

---
+++ src/main/java/com/ljw/test/App.java
@@ -0,0 +1,13 @@
+package com.ljw.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
